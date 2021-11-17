124d1c6ae08348ecfbbd93416873856e924dd88a
James Sully <sullyj3@gmail.com>
2021-11-17 15:12:46 +1100

    Softer clipping

--- app/Main.hs
+++ app/Main.hs
@@ -43,31 +43,25 @@ main = do
       f4 = sinusoid 0.4 7 2.4 0
       fSum x = f1 x + f2 x + f3 x + f4 x
 
+      f = bipolarToUnipolar . tanh . fSum
+
   for_ [0.01, 0.02 ..] \x -> do
     width <- readTVarIO widthVar
-    T.putStrLn $ nth width (quantize width fSum x) '.'
+    T.putStrLn $ nth width (quantize width . f $ x) '.'
     sleep 0.01
 
 sinusoid a b h k x = a * sin (b * (x-h)) + k
 
--- >>> sin (pi/2)
--- 1.0
-
--- >>> quantize 1000 sin (pi/2)
--- 999
-
--- >>> sin (3*pi/2)
--- -1.0
-
--- >>> quantize 1000 sin (3*pi/2)
--- 0
-
--- take a function with range [-1,1], quantize it and scale it to [0,steps)
-quantize :: Int -> (Double -> Double) -> (Double -> Int)
-quantize steps f x = round $ (fromIntegral (steps-1)/2) * (f x + 1.0)
+-- take a function with range [0,1], quantize it and scale it to [0,steps)
+quantize :: Int -> Double -> Int
+quantize steps x = round $ (fromIntegral (steps-1)) * x
 
 nth :: Int -> Int -> Char -> Text
 nth width n c
   | n >= width = T.replicate width " "
   | otherwise  = T.replicate n " " <> T.singleton c <> T.replicate (width - n - 1) " "
 
+-- not sure if this is the right terminology - takes functions with range
+-- [-1,1] to [0,1]
+bipolarToUnipolar :: Double -> Double
+bipolarToUnipolar x = (x / 2) + 0.5
